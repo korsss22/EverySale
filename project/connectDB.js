d5d967c45b8e66b40365b5f424bbb469112ae708
Byeongjun <byeongjun1326@gmail.com>
2024-06-01 19:12:45 +0900

    서버 오픈 테스트

--- project/connectDB.js
+++ project/connectDB.js
@@ -4,11 +4,11 @@ const oracledb = require("oracledb");
 async function connectDB() {
   let connection;
   try {
-    // OracleDB에 연결
+    // OracleDB에 연결                           //객체 사용
     connection = await oracledb.getConnection({
       user: "sys", //오라클 DB ID
       password: "1234", //오라클 설치할때 PASSWORD
-      connectString: "localhost:1521/xe", //포트 : 1521 SID : xe
+      connectString: "192.168.35.50:1521/xe", //포트 : 1521 SID : xe
       privilege: oracledb.SYSDBA,
     });
 
