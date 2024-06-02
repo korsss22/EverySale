507f32c825ea779fdaa3b71c0aa2e3de93d0dc1e
byeongjun2 <87219189+korsss22@users.noreply.github.com>
2024-06-02 16:40:50 +0900

    Update connectDB.js

--- project/connectDB.js
+++ project/connectDB.js
@@ -6,9 +6,9 @@ async function connectDB() {
   try {
     // OracleDB에 연결                           //객체 사용
     connection = await oracledb.getConnection({
-      user: "c##byeong", //오라클 DB ID
+      user: "localhost", //오라클 DB ID
       password: "1234", //오라클 설치할때 PASSWORD
-      connectString: "192.168.35.50:1521/xe", //포트 : 1521 SID : xe
+      connectString: "localhost/xe", //포트 : 1521 SID : xe
       //privilege: oracledb.SYSDBA,
     });
 
